478b070475195e1d582c6e647839859384f75f09
Michael Sliger <michaelj.sliger@gmail.com>
2020-12-04 16:41:26 -0700

    content.js now holds 3 temp projects

--- src/Content/Content.js
+++ src/Content/Content.js
@@ -5,7 +5,19 @@ class Content extends React.Component {
     render() {
         return (
             <div>
-                UNDER CONSTRUCTION
+                <div>
+                    UNDER CONSTRUCTION:: Expected before 12/9
+                </div>
+                <div>
+                    In the meantime, here are my recent projects:
+                    <br />
+                    <br />
+                    <ul>
+                        <li><a href="https://gofish-team2.vercel.app/">Multi-Player Go-Fish (Full-Stack)</a></li>
+                        <li><a href="https://spaced-rep-frontend.vercel.app/">Spaced Repetition Language Learning Platform (Full-Stack)</a></li>
+                        <li><a href="https://capstone-sliger.vercel.app/">Satire Store (Full-Stack)</a></li>
+                    </ul>
+                </div>
             </div>
         )
     }
