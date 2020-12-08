23f27232f0e5d770cf8af8958959b51de0b090c6
Michael Sliger <michaelj.sliger@gmail.com>
2020-12-07 18:14:31 -0700

    added alt tag to images

--- src/Slideshow/Slideshow.js
+++ src/Slideshow/Slideshow.js
@@ -36,7 +36,7 @@ class ImageSlideshow extends React.Component {
         const displayedImage = this.state.images[this.state.index]
         return (
             <div className="slideshow-container">
-                {displayedImage ? <img src={displayedImage} /> : ''}
+                {displayedImage ? <img src={displayedImage} alt="Project Image"/> : ''}
                 {this.state.images.length > 1 ? (
                     <>
                         <a className="prev" onClick={() => this.plusSlides(-1)}>&#10094;</a>
