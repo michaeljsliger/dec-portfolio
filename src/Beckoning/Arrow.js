c07b900556796684a983968942548d78376ce0eb
Michael Sliger <michaelj.sliger@gmail.com>
2020-12-09 16:42:23 -0700

    revisions pt 1

--- src/Beckoning/Arrow.js
+++ src/Beckoning/Arrow.js
@@ -4,7 +4,10 @@ import './Arrow.css';
 function BeckoningArrow(props) {
 
     return (
-        <div id="fixeddiv"><i className="down big-arrow"></i></div>
+        <div id="fixeddiv" className="tooltip">
+            <div className="tooltiptext">Scroll down!</div>
+            <i className="down big-arrow"></i>
+        </div>
     )
 }
 
